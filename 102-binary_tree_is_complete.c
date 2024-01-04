a72c33c31be86f1ecfe3c9ace6daaa2c7b5bbf1d
mahmoud <information.mahmoud@gmail.com>
2024-01-04 21:22:53 +0200

    ok

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,75 +1,62 @@
 #include "binary_trees.h"
 
 /**
- * enqueue - Adds a node to the end of the queue
- * @queue_head: Pointer to the head of the queue
- * @node: Node to be enqueued
- *
- * Return: Pointer to the new queue node
- */
-binary_tree_queue_t *enqueue(binary_tree_queue_t *queue_head,
-							 const binary_tree_t *node)
-{
-	binary_tree_queue_t *new_node = malloc(sizeof(binary_tree_queue_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->node = node;
-	new_node->next = NULL;
-
-	if (queue_head == NULL)
-		return (new_node);
-
-	binary_tree_queue_t *temp = queue_head;
-
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new_node;
-	return (new_node);
-}
-
-/**
  * binary_tree_is_complete - Checks if a binary tree is complete
- * @tree: A pointer to the root node of the tree to check
+ * @tree: Pointer to the root node of the tree to check
  *
- * Return: 1 if the tree is complete, 0 otherwise
+ * Return: If tree is NULL, your function must return 0.
+ * In a complete binary tree every level, except possibly the last,
+ * is completely filled, and all nodes in the last level
+ *  are as far left as possible.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	binary_tree_queue_t *queue_head = NULL;
-	int flag = 0;
+	int node_count = binary_tree_size(tree);
 
-	queue_head = enqueue(queue_head, tree);
+	return (is_complete(tree, 0, node_count));
+}
 
-	while (queue_head)
-	{
-		const binary_tree_t *current = queue_head->node;
+/**
+ * is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * @index: index
+ * @node_count: node count
+ * Return: If tree is NULL, your function must return 0.
+ * In a complete binary tree every level, except possibly the last,
+ * is completely filled, and all nodes in the last level
+ *  are as far left as possible.
+ */
 
-		if (current->left)
-		{
-			if (flag == 1)
-				return (0);
-			queue_head = enqueue(queue_head, current->left);
-		}
-		else
-			flag = 1;
+int is_complete(const binary_tree_t *tree, int index, int node_count)
+{
+	if (tree == NULL)
+		return (1);
 
-		if (current->right)
-		{
-			if (flag == 1)
-				return (0);
-			queue_head = enqueue(queue_head, current->right);
-		}
-		else
-			flag = 1;
+	if (index >= node_count)
+		return (0);
 
-		queue_head = queue_head->next;
-	}
+	return (is_complete(tree->left, 2 * index + 1, node_count) &&
+			is_complete(tree->right, 2 * index + 2, node_count));
+}
 
-	return (1);
+/**
+ * binary_tree_size - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: If tree is NULL, your function must return 0.
+ * In a complete binary tree every level, except possibly the last,
+ * is completely filled, and all nodes in the last level
+ *  are as far left as possible.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -76,5 +76,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_queue_t *enqueue(binary_tree_queue_t *queue_head,
+							 const binary_tree_t *node);
+
+int is_complete(const binary_tree_t *tree, int index, int node_count);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /*Binary trees header*/
