221663787da4417b3fce2cde83afdac53b596e37
mahmoud <information.mahmoud@gmail.com>
2024-01-04 18:54:42 +0200

    ok

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,13 +11,13 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	new_node = binary_tree_node(parent, value);
 
 	/* Check if parent is NULL */
 	if (parent == NULL)
 		return (NULL);
 
 	/* Create a new node */
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
