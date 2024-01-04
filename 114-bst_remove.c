be6abb782e0580593df6095a6993e753d847a503
mahmoud <information.mahmoud@gmail.com>
2024-01-04 23:14:03 +0200

    ok

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+bst_t *bst_remove(bst_t *root, int value);
+bst_t *remove_recursive(bst_t *root, bst_t *node, int value);
+bst_t *delete_node(bst_t *root, bst_t *node);
+bst_t *inorder_successor_node(bst_t *root);
 /**
  * remove_node - Removes a node from a Binary Search Tree.
  *
@@ -8,7 +12,7 @@
  *
  * Return: A pointer to the new root node of the tree after the removal.
  */
-bst_t *remove_node(bst_t *root, int value)
+bst_t *bst_remove(bst_t *root, int value)
 {
 	return (remove_recursive(root, root, value));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -90,7 +90,6 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
-bst_t *remove_node(bst_t *root, int value);
 bst_t *remove_recursive(bst_t *root, bst_t *node, int value);
 bst_t *delete_node(bst_t *root, bst_t *node);
 bst_t *inorder_successor_node(bst_t *root);
