6c5e69ad680e624f6492bc86448fb0e0cb87036f
mahmoud <information.mahmoud@gmail.com>
2024-01-04 22:46:55 +0200

    yupp

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,29 +1,39 @@
 #include "binary_trees.h"
 
 /**
+ * newNode - Builds a Binary Search Tree (BST) from an array
+ * @value: Pointer to the first element of the array to be converted
+ *
+ * Return: Pointer to the root node of the created BST, or NULL on failure
+ */
+bst_t *newNode(int value)
+{
+	bst_t *node = (bst_t *)malloc(sizeof(bst_t));
+
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+/**
  * array_to_bst - Builds a Binary Search Tree (BST) from an array
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
  *
  * Return: Pointer to the root node of the created BST, or NULL on failure
  */
+
 bst_t *array_to_bst(int *array, size_t size)
 {
-	bst_t *root, *state;
+	bst_t *root;
 
-	if (array == NULL || size == 0)
+	if (size == 0)
 		return (NULL);
 
-	root = NULL;
-
-	for (size_t i = 0; i < size; ++i)
-	{
-
-		state = bst_insert(&root, array[i]);
-
-		if (state == NULL)
-			continue;
-	}
+	root = newNode(array[0]);
+	for (size_t i = 1; i < size; i++)
+		bst_insert(&root, array[i]);
 
 	return (root);
 }
--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -8,7 +8,6 @@
  * Return: Once located, the node containing a value equals
  *  to value must be removed and freed.
  */
-
 bst_t *bst_remove(bst_t *root, int value)
 {
 	bst_t *temp;
@@ -26,39 +25,46 @@ bst_t *bst_remove(bst_t *root, int value)
 	}
 	else
 	{
-		if (root->left == NULL)
+		if (!root->left || !root->right)
 		{
-			temp = root->right;
-			free(root);
-			return (temp);
-		}
-		else if (root->right == NULL)
-		{
-			temp = root->left;
-			free(root);
-			return (temp);
-		}
+			temp = root->left ? root->left : root->right;
 
-		temp = minValueNode(root->right);
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
+			if (temp == NULL)
+			{
+				temp = root;
+				root = NULL;
+			}
+			else
+				*root = *temp;
+			free(temp);
+		}
+		else
+		{
+
+			temp = root->right;
+			while (temp->left != NULL)
+				temp = temp->left;
+
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+		}
 	}
 	return (root);
 }
 
 /**
- * minValueNode - Removes a node from a Binary Search Tree
+ * findMin - Removes a node from a Binary Search Tree
  * @node: Pointer to the root node of the tree where you will remove a node
  *
  * Return: Once located, the node containing a value equals to
  *  value must be removed and freed.
  */
 
-bst_t *minValueNode(bst_t *node)
+bst_t *findMin(bst_t *node)
 {
-	bst_t *current = node;
-
-	while (current && current->left != NULL)
-		current = current->left;
-	return (current);
+	while (node->left != NULL)
+	{
+		node = node->left;
+	}
+	return (node);
 }
