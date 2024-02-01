d1866b24676aef508d1abfcff0383a29f05c31ef
mahmoud malek <information.mahmoud@gmail.com>
2024-02-01 19:13:22 +0200

    udpate

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,25 +10,19 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second)
 {
-	const binary_tree_t *temp;
-	const binary_tree_t *temp2;
+	const binary_tree_t *temp1 = first;
+	const binary_tree_t *temp2 = second;
 
 	/* Handle edge cases where either of the nodes is NULL */
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	/* Store the first node to traverse upwards */
-	temp = first;
-	temp2 = second;
 	/* Traverse upwards from the first node */
-	while (temp && temp2)
+	while (temp1 != temp2)
 	{
-		if (temp == temp2 || temp->parent == temp2 || temp2->parent == temp)
-			return ((binary_tree_t *)temp);
-
-		temp = temp->parent;
-		temp2 = temp2->parent;
+		temp1 = (temp1 == NULL ? second : temp1->parent);
+		temp2 = (temp2 == NULL ? first : temp2->parent);
 	}
 
-	return (NULL); /* No common ancestor found */
+	return ((binary_tree_t *)temp1);
 }
